4d0cee858c3f683b065389a1546025f561050827
SaadDAHMANI <TahatAtakor7@gmail.com>
2020-07-11 22:59:47 +0100

    update...
    Signed-off-by: SaadDAHMANI <TahatAtakor7@gmail.com>

--- IrrigationNetConsole/Program.cs
+++ IrrigationNetConsole/Program.cs
@@ -11,7 +11,8 @@ namespace IrrigationNetConsole
              string fileName=string.Format(@"{0}\Data\crops\FAO\TOMATO.CRO", AppDomain.CurrentDomain.BaseDirectory);
              Console.WriteLine("The directory is :");
              Console.WriteLine(fileName);
-           
+             
+              
 
              IOLib.FileReader reader  = new IOLib.FileReader(fileName);
              IOCrop crop=reader.Read();
