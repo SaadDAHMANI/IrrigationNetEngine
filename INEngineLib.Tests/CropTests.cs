d09d04acbacc939835663737fd4bb82e66cd3724
SaadDAHMANI <TahatAtakor7@gmail.com>
2020-07-14 16:48:46 +0100

    Crop.HarvestDate when Crop is 10 and 160 days
    Signed-off-by: SaadDAHMANI <TahatAtakor7@gmail.com>

---
+++ INEngineLib.Tests/CropTests.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+using INEngineLib;
+
+namespace INEngineLib.Tests
+{
+    public class CropTests
+    {
+
+        //
+        //Crop under test.
+        //  
+         Crop Crop_uts;        
+         
+         //constructur:
+         public CropTests()
+         {
+             Crop_uts= new Crop();
+         }
+
+        [Fact]
+        public void HarvestDateWhenCropLifIs10Days()
+        {
+            Crop_uts.PlantingDate=Convert.ToDateTime("#07/15/2020#").AddDays(10);
+            DateTime predictedHarvestDate = Convert.ToDateTime("#07/25/2020#");
+            Assert.Equal(predictedHarvestDate, Crop_uts.HarvestDate);
+        }
+        [Fact] 
+        public void HarvestDateWhenCropLifIs160Days()
+        {
+             Crop_uts.PlantingDate=Convert.ToDateTime("#07/15/2020#");
+             Crop_uts.InitialStage =40;
+             Crop_uts.DevelopmentStage =40;
+             Crop_uts.MidSeasonStage=40 ;
+             Crop_uts.LateSeasonStage=40;
+            DateTime predictedHarvestDate = Convert.ToDateTime("#12/22/2020#");
+            Assert.Equal(predictedHarvestDate, Crop_uts.HarvestDate);
+        }
+    }
+}
---
+++ INEngineLib.Tests/SoilTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using INEngineLib;
+
+namespace INEngineLib.Tests
+{
+    public class SoilTests
+    {   
+        private Soil Soil_uts;
+        public SoilTests()
+        {
+             Soil_uts= new Soil();
+        }
+
+
+    }
+}
